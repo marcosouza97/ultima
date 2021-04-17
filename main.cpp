9fcd2f0dfcae7c1226932a6afde11cb8704880ca
Marcos Alves <>
2021-04-17 20:16:25 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+
+  int num1,num2;
+  string caracter1,caracter2;
+
+  cout << "Qual tamanho da sua palavra?" << endl;
+  cin >> num1;
+
+  cout << "digite o caracter de identificação"<<endl;
+  cin >> caracter1;
+
+  cout << "digite qual é a tecla " << endl;
+  cin >> num2;
+
+  cout << "digite o segundo caracter de identificação" << endl;
+  cin >> caracter2;
+
+  cout << "digite quantas vezes a tecla vai ser precionada"<<endl;
+
+  if(num1 >0 && num1 <50 && caracter1 == "#" && caracter2 == "="){
+      
+  }
+  else{
+    cout << "Uma ou mais informações invalida" << endl;
+  }
+
+
+
+
+
+
+  
+  
+}
